69f06492e6e22f98a180e94c86f355d774ebf0a8
RK <rktan058@gmail.com>
2026-04-09 13:35:23 +0800

    Add and delete function version 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,137 @@
 #include<iostream>
+#include<fstream>
+
+
 using namespace std;
 
-void main()
+//declaration
+int choice;
+
+//stack
+
+
+
+struct Book
 {
+    int id;
+    string author,title;
+};
+
+struct Node
+{
+    Book data;
+    Node* next;
+};
+
+
+//save to file
+void saveToFile(Node* head) {
+    ofstream outFile("books.txt");
+    Node* temp = head;
+    while (temp != nullptr) {
+        outFile << temp->data.id << "\t"
+                << temp->data.title << "\t"
+                << temp->data.author << endl;
+        temp = temp->next;
+    }
+    outFile.close();
+}
+
+
+//add
+void add(Node*& head) {
+    Node* newNode = new Node(); // Dynamic memory allocation
+
+    cout << "Enter id: ";
+    cin >> newNode->data.id;
+    cin.ignore();
+    cout << "Enter Title: ";
+    getline(cin, newNode->data.title);
+    cout << "Enter author: ";
+    getline(cin, newNode->data.author);
+
+    // Points the new book to the current head, then makes this the new head
+    newNode->next = head;
+    head = newNode;
+
+    saveToFile(head);
+    cout << "Book added successfully.\n";
+}
+
+
+//delete
+void delt(Node*& head) {
+    if (head == nullptr) return;
+
+    int searchID;
+    cout << "Enter ID to delete: ";
+    cin >> searchID;
+
+    Node* temp = head;
+    Node* prev = nullptr;
+
+    // If the book to delete is the head
+    if (temp != nullptr && temp->data.id == searchID) {
+        head = temp->next;
+        delete temp; // Free memory
+        saveToFile(head);
+        return;
+    }
+
+    // Search for the ID
+    while (temp != nullptr && temp->data.id != searchID) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == nullptr) return;
+
+    // Unlink the node and free memory
+    prev->next = temp->next;
+    delete temp;
+    saveToFile(head);
+}
+
+
+
+int main()
+{
+    Node* head = nullptr;
+
+
+        do
+        {
+            cout<<"\n ------------Libary system------------ \n";
+            cout<<"1. Add Book \n";
+            cout<<"2. Delete Book \n";
+            cout<<"3. Edit Book details \n";
+            cout<<"4. Display all books \n";
+            cout<<"5. Search Book \n";
+            cout<<"6. Exit\n";
+            cout<<"Enter a number: ";
+            cin>>choice;
+
+            if(choice >0 && choice < 6)
+            {
+                switch(choice)
+                {
+                    case 1:add(head); break;
+                    case 2:delt(head); break;
+
+
+                }
+            }
+            else if (choice == 6)
+            {
+                break;
+            }
+            else
+            {
+                cout<<"Invalid choice.";
+            }
+
+
+        }while (choice != 6);
+
 
 }
